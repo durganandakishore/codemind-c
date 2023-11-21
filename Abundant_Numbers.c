3ef12291dd566efda7cadbd3b2ed84baffb00496
durganandakishore <147476030+durganandakishore@users.noreply.github.com>
2023-11-21 11:01:08 +0530

    #include<stdio.h> int main() {     int i,a,b=0,c=1,d;     scanf("%d",&a);     printf("%d %d"b,c);     for(i=1;i<=a;i++)     {         d=b+c;         b=c;         c=d;     }     if()     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,a,b=0,c=1,d;
+    scanf("%d",&a);
+    printf("%d %d"b,c);
+    for(i=1;i<=a;i++)
+    {
+        d=b+c;
+        b=c;
+        c=d;
+    }
+    if()
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
