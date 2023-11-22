f5884d99e4d786ac53eeb575249add6df8cc0243
durganandakishore <147476030+durganandakishore@users.noreply.github.com>
2023-11-22 10:17:43 +0530

    #include<stdio.h> int main() {     int n,x;     scanf("%d%d",&n,&x);     if(((x*(x+1))/2)>n)     {         printf("NO");     }     else     {         printf("YES");     } }

---
+++ Sharing_is_Caring.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,x;
+    scanf("%d%d",&n,&x);
+    if(((x*(x+1))/2)>n)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
+}
