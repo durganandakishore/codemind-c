f2d3523b56a86f149b660c7913ace22c1e72f297
durganandakishore <147476030+durganandakishore@users.noreply.github.com>
2024-02-20 09:43:15 +0530

    #include<stdio.h> #include<string.h> int main() {     char s1[101];     char s2[101];     scanf("%[^ ]",&s1);   //  int n=strcpy(s1);     strcpy(s2,s1);     printf("%s",s2); }

---
+++ String_Copy.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[101];
+    char s2[101];
+    scanf("%[^
+]",&s1);
+  //  int n=strcpy(s1);
+    strcpy(s2,s1);
+    printf("%s",s2);
+}
